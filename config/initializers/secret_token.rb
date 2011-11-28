2063ca9a3ad944f53cdd2f01798af4da43538185
Cody Marcel <cody.marcel@gmail.com>
2011-11-28 15:49:34 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Grievebot::Application.config.secret_token = '6eb2fa47b5abf8aa86ed112bd8cbf46a95ef6d549ec08898553dcb4a411c05a1b6abc1e2120a011e8b7b2dea9d6f661a17c83d1cdf5bfb52de0d7e6fa9c1079d'
