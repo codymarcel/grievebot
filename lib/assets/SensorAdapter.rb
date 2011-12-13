4d4572acd1bbb478e770103db052506d68828870
Matthew Peterson <mateo42@gmail.com>
2011-12-12 17:31:54 -0800

    oh holy fucking shit balls batman

--- lib/assets/SensorAdapter.rb
+++ lib/assets/SensorAdapter.rb
@@ -1,22 +1,30 @@
 require 'rubygems'
 require 'httparty'
 require 'cgi'
+require 'uri'
 
 class SensorAdapter
   include HTTParty
   @authResults = nil
+  @refreshResults = nil
   
   format :json
 
   #authenticate
   def self.authenticate
+
     base_uri = ENV['SALESFORCE_OAUTH2_URI']
-    base_auth_query = 'grant_type=password&client_id='+ENV['SALESFORCE_OAUTH2_KEY']+
+    base_auth_query = 
+      'grant_type=password'+
+      '&client_id='+ENV['SALESFORCE_OAUTH2_KEY']+
       '&client_secret='+ENV['SALESFORCE_OAUTH2_SECRET']+
       '&username='+ENV['SALESFORCE_OAUTH2_USERNAME']+
       '&password='+ENV['SALESFORCE_OAUTH2_PASSWORD']
     @authResults = post(base_uri, { :body => base_auth_query })
     puts @authResults.to_yaml
+    
+
+    
     return @authResults
   end
 
@@ -24,42 +32,19 @@ class SensorAdapter
     if !@authResults
       SensorAdapter.authenticate
     end
+    oauth_header = 'OAuth ' + @authResults['access_token']#[16,@authResults['access_token'].length])
+    puts oauth_header
    
-    headers 'Authorization' => 'OAuth ' + @authResults["access_token"]
+    headers 'Authorization' => oauth_header
     headers 'Content-Type' => 'application/json'
-    reading_uri = @authResults["instance_url"] + '/services/data/v23.0/' + query_stuff
+    reading_uri = @authResults['instance_url'] + '/services/data/v23.0/' + query_stuff
     
     puts "Querying: " + reading_uri
     
-    ret = HTTParty.get(reading_uri)
+    ret = get(reading_uri)
     
     return ret
     
-  end
-
-  #update my own personal status
-  def self.update_status(device, measure, reading)
-    if !@authResults
-      SensorAdapter.authenticate
-    end
-    
-    #now put to readings
-    headers 'Authorization' => 'OAuth ' + @authResults["access_token"]
-    headers 'Content-Type' => 'application/json'
-    reading_uri = 'https://na1.salesforce.com/services/data/v20.0/sobjects/Reading__c/'
-    
-    newReading = '{ "Device__c" : "'+device +'", "Measure__c" : "' + measure + '", "Value__c" : "'+reading+'" }'
-    
-    ret = post(reading_uri, { :body => newReading })
-    
-    puts "PPOSTTTED**********"
-    puts ret
-  end
-  
-  def self.handle_command(type, param1, param2)
-    url = "http://10.0.1.254/" + type + "/" + param1 + "/" + param2
-    puts("url ", url)
-    ret = get(url);
   end
 
 end
