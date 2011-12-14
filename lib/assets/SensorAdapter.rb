b02b93c97372726192de27baa1a486e7fe570ecd
Matthew Peterson <mateo42@gmail.com>
2011-12-13 20:30:37 -0800

    show the latest chatter post from AoGOL

--- lib/assets/SensorAdapter.rb
+++ lib/assets/SensorAdapter.rb
@@ -23,28 +23,33 @@ class SensorAdapter
     @authResults = post(base_uri, { :body => base_auth_query })
     puts @authResults.to_yaml
     
-
-    
     return @authResults
   end
+  
+  def self.api_query(query)
+    return send_query(query, 0)
+  end
 
-  def self.api_query(query_stuff)
+  def self.send_query(query, try)
     if !@authResults
       SensorAdapter.authenticate
     end
-    oauth_header = 'OAuth ' + @authResults['access_token']
-    puts oauth_header
-   
+    oauth_header = 'OAuth ' + @authResults['access_token']  
     headers 'Authorization' => oauth_header
     headers 'Content-Type' => 'application/json'
-    reading_uri = @authResults['instance_url'] + '/services/data/v23.0/' + query_stuff
-    
-    puts "Querying: " + reading_uri
-    
+    reading_uri = @authResults['instance_url'] + query 
     ret = get(reading_uri)
     
-    return ret
+    if ret.include? 'errorCode'
+      if try == 0
+        SensorAdapter.authenticate
+        send_query(query, 1)
+      else
+        puts "ERROR MAKING QUERY"
+        puts ret.to_yaml
+      end
+    end
     
+    return ret    
   end
-
 end
