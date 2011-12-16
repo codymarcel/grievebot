c7a724601338512da5830b4109f2f18bb5929bf5
Cody Marcel <cody.marcel@gmail.com>
2011-12-15 18:59:34 -0800

    Adding localtunnel

---
+++ app/controllers/localtunnel_controller.rb
@@ -0,0 +1,6 @@
+class LocaltunnelController < ApplicationController
+  def establish_tunnel
+    output = `localtunnel 3000`
+    puts output + "lalala"
+  end
+end
---
+++ test/unit/helpers/localtunnel_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class LocaltunnelHelperTest < ActionView::TestCase
+end
