6377723d414e7dc94cfb91942caa738e6eb576e5
Cody Marcel <cody.marcel@gmail.com>
2011-12-30 10:45:10 -0800

    removed extra arduino

--- GrieveBotArduinoCody/dns.h
+++
@@ -1,9 +0,0 @@
-// Just a utility function to nicely format an IP address.
-char* ip_to_str(IPAddress ipAddr)
-{
-
-  static char buf[16];
-  sprintf(buf, "%d.%d.%d.%d\n\0", ipAddr[0], ipAddr[1], ipAddr[2], ipAddr[3]);
-  return buf;
-}
-
