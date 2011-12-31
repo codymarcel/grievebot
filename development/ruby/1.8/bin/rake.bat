65c48561e45c187d423d9e04a8cc6bd38694fb06
Matthew Peterson <mateo42@gmail.com>
2011-12-31 15:15:01 -0800

    removing developement directory

--- development/ruby/1.8/bin/rake.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-IF NOT "%~f0" == "~f0" GOTO :WinNT
-@"ruby.exe" "C:/Users/cody/Documents/Aptana Studio 3 Workspace/grievebot/development/ruby/1.8/bin/rake" %1 %2 %3 %4 %5 %6 %7 %8 %9
-GOTO :EOF
-:WinNT
-@"ruby.exe" "%~dpn0" %*
